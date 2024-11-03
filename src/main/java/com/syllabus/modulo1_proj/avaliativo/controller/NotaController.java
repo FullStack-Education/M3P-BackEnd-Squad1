069b7acbda49389d7a376ac0d64bae59ec51adc0
David Alves Dutra <dadutra@hotmail.com>
2024-11-03 20:00:45 -0300

    Swagger para Notas Controller.

--- src/main/java/com/syllabus/modulo1_proj/avaliativo/controller/NotaController.java
+++ src/main/java/com/syllabus/modulo1_proj/avaliativo/controller/NotaController.java
@@ -2,6 +2,8 @@ package com.syllabus.modulo1_proj.avaliativo.controller;
 import com.syllabus.modulo1_proj.avaliativo.dtoUtils.notas.DtoNota;
 import com.syllabus.modulo1_proj.avaliativo.dtoUtils.notas.DtoNotaResponse;
 import com.syllabus.modulo1_proj.avaliativo.service.NotaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:4200", "https://viacep.com.br/ws/null/json/"})
+@CrossOrigin(origins = { "http://localhost:4200", "https://viacep.com.br/ws/null/json/"})
 @RestController
 @RequestMapping("notas")
+@Api(value = "Notas", tags = "Notas")
 public class NotaController {
 
     private static final Logger logger = LoggerFactory.getLogger(NotaController.class);
@@ -26,24 +29,28 @@ public class NotaController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Criar uma nova nota")
     public ResponseEntity<DtoNotaResponse> criarNota(@RequestBody @Valid DtoNota novaNota) {
         logger.info("Soliciada inserção de nova Nota.");
         return ResponseEntity.status(HttpServletResponse.SC_CREATED).body(service.criarNota(novaNota));
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Obter nota por ID")
     public ResponseEntity<DtoNotaResponse> obterNotaPorId(@PathVariable Long id) {
         logger.info("Soliciada Nota por ID, {}.", id);
         return ResponseEntity.status(HttpStatus.OK).body(service.obterNotaPorId(id));
     }
 
     @PutMapping("{id}")
+    @ApiOperation(value = "Atualizar nota por ID")
     public ResponseEntity<DtoNotaResponse> atualizarNota(@PathVariable Long id, @RequestBody @Valid DtoNota nota) {
         logger.info("Soliciada alteração de Nota, ID {}.", id);
         return ResponseEntity.status(HttpStatus.OK).body(service.atualizarNota(id, nota));
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deletar nota por ID")
     public ResponseEntity<Void> deletarNota(@PathVariable Long id) {
         logger.info("Soliciada exclusão de Nota, ID {}.", id);
         service.deletarNota(id);
