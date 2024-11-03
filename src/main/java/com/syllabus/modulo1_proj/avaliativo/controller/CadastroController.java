fc7c230ee3824eb1ca57cd421c53d4d88558846d
David Alves Dutra <dadutra@hotmail.com>
2024-11-03 19:40:38 -0300

    Swagger para Cadastro Controller.

--- src/main/java/com/syllabus/modulo1_proj/avaliativo/controller/CadastroController.java
+++ src/main/java/com/syllabus/modulo1_proj/avaliativo/controller/CadastroController.java
@@ -2,6 +2,9 @@ package com.syllabus.modulo1_proj.avaliativo.controller;
 import com.syllabus.modulo1_proj.avaliativo.dtoUtils.usuario.DtoUsuarioRequest;
 import com.syllabus.modulo1_proj.avaliativo.dtoUtils.usuario.DtoUsuarioResponse;
 import com.syllabus.modulo1_proj.avaliativo.service.UsuarioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -9,7 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:4200", "https://viacep.com.br/ws/null/json/"})
+@CrossOrigin(origins = { "http://localhost:4200", "https://viacep.com.br/ws/null/json/" })
+@Api(value = "Cadastro Controller", tags = {"Cadastro"})
 @RestController
 @RequestMapping("cadastro")
 public class CadastroController {
@@ -22,10 +26,11 @@ public class CadastroController {
         this.service = service;
     }
 
+    @ApiOperation(value = "Criar um novo usuário", response = DtoUsuarioResponse.class)
     @PostMapping
-    public ResponseEntity<DtoUsuarioResponse> criarUsuario(@RequestBody @Valid DtoUsuarioRequest novoUsuario) {
+    public ResponseEntity<DtoUsuarioResponse> criarUsuario(
+            @ApiParam(value = "Dados do novo usuário", required = true) @RequestBody @Valid DtoUsuarioRequest novoUsuario) {
         logger.info("Solicitado cadastramento de NOVO Usuário.");
         return ResponseEntity.status(HttpServletResponse.SC_CREATED).body(service.criarUsuario(novoUsuario));
     }
-
 }
