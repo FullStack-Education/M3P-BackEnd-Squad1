dc92f55cf13f5292a9f81517702370102c90021a
David Alves Dutra <dadutra@hotmail.com>
2024-11-03 20:44:01 -0300

    Ajuste de config swagger.

--- src/main/java/com/syllabus/modulo1_proj/avaliativo/swagger/SwaggerConfig.java
+++ src/main/java/com/syllabus/modulo1_proj/avaliativo/swagger/SwaggerConfig.java
@@ -8,9 +8,73 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
+    public GroupedOpenApi turmasApi() {
         return GroupedOpenApi.builder()
-                .group("public")
+                .group("turmas")
+                .pathsToMatch("/turmas/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi materiasApi() {
+        return GroupedOpenApi.builder()
+                .group("materias")
+                .pathsToMatch("/materias/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi docentesApi() {
+        return GroupedOpenApi.builder()
+                .group("docentes")
+                .pathsToMatch("/docentes/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi alunosApi() {
+        return GroupedOpenApi.builder()
+                .group("alunos")
+                .pathsToMatch("/alunos/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi cursosApi() {
+        return GroupedOpenApi.builder()
+                .group("cursos")
+                .pathsToMatch("/cursos/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi loginApi() {
+        return GroupedOpenApi.builder()
+                .group("login")
+                .pathsToMatch("/login/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi notasApi() {
+        return GroupedOpenApi.builder()
+                .group("notas")
+                .pathsToMatch("/notas/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi cadastroApi() {
+        return GroupedOpenApi.builder()
+                .group("cadastro")
+                .pathsToMatch("/cadastro/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi dashboardApi() {
+        return GroupedOpenApi.builder()
+                .group("dashboard")
                 .pathsToMatch("/dashboard/**")
                 .build();
     }
