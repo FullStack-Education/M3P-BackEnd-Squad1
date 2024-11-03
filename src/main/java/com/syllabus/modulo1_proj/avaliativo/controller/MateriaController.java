940321ba90dfb6a3b9bc7275d69adea44f1b39fc
David Alves Dutra <dadutra@hotmail.com>
2024-11-03 19:59:23 -0300

    Swagger para Materias Controller.

--- src/main/java/com/syllabus/modulo1_proj/avaliativo/controller/MateriaController.java
+++ src/main/java/com/syllabus/modulo1_proj/avaliativo/controller/MateriaController.java
@@ -2,6 +2,8 @@ package com.syllabus.modulo1_proj.avaliativo.controller;
 import com.syllabus.modulo1_proj.avaliativo.dtoUtils.DtoMateria;
 import com.syllabus.modulo1_proj.avaliativo.entities.Materia;
 import com.syllabus.modulo1_proj.avaliativo.service.MateriaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:4200", "https://viacep.com.br/ws/null/json/"})
+@CrossOrigin(origins = { "http://localhost:4200", "https://viacep.com.br/ws/null/json/"})
 @RestController
 @RequestMapping("materias")
+@Api(value = "Materias", tags = "Materias")
 public class MateriaController {
 
     private static final Logger logger = LoggerFactory.getLogger(MateriaController.class);
@@ -26,30 +29,35 @@ public class MateriaController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Criar uma nova matéria")
     public ResponseEntity<Materia> criarMateria(@RequestBody @Valid DtoMateria novaMateria) {
         logger.info("Requerido o cadastro de nova Materia.");
         return ResponseEntity.status(HttpServletResponse.SC_CREATED).body(service.criarMateria(novaMateria));
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Obter matéria por ID")
     public ResponseEntity<Materia> obterMateriaPorId(@PathVariable Long id) {
         logger.info("Solicitada Matéria por ID, {}", id);
         return ResponseEntity.status(HttpStatus.OK).body(service.obterMateriaPorId(id));
     }
 
     @GetMapping()
+    @ApiOperation(value = "Listar todas as matérias")
     public ResponseEntity<List<Materia>> listarTodasMaterias() {
         logger.info("Solicitada listagem de materias");
         return ResponseEntity.status(HttpStatus.OK).body(service.listarTodasMaterias());
     }
 
     @PutMapping("{id}")
+    @ApiOperation(value = "Atualizar matéria por ID")
     public ResponseEntity<Materia> atualizarMateria(@PathVariable Long id, @RequestBody @Valid DtoMateria materia) {
         logger.info("Requerida alteração da Materia ID {}", id);
         return ResponseEntity.status(HttpStatus.OK).body(service.atualizarMateria(id, materia));
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deletar matéria por ID")
     public ResponseEntity<Void> deletarMateria(@PathVariable Long id) {
         logger.info("Solicitada a exclusão de Matéria, ID {}", id);
         service.deletarMateria(id);
